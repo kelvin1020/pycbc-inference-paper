adf99fe20760f4511793f064ee1dafb721a00940
Shucheng Yang <ysc@shao.ac.cn>
2019-08-20 22:12:14 +0800

    install

---
+++ run_files/GW150914/install_virtualEnv.sh
@@ -0,0 +1,4 @@
+# conda create -n pycbc_v1_12_3 python=2.7
+# conda activate pycbc_v1_12_3
+pip install -r requirements.txt
+python -m ipykernel install --user --name pycbc_v1_12_3 --display-name pycbc_v1_12_3
