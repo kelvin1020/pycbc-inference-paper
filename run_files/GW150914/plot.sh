dd30bb8bd357c2248bbb5c4c0ebd553a95df0c86
Shucheng Yang <ysc@shao.ac.cn>
2019-08-20 20:36:42 +0800

    gw150914_low_resolution

---
+++ run_files/GW150914/plot.sh
@@ -0,0 +1,29 @@
+pycbc_inference_plot_posterior --verbose \
+    --input-file /home/ysc/Documents/pycbc-inference-paper/run_files/GW150914/gw150914_posteriors_thinned.hdf \
+    --output-file q_chieff_gw150914.png \
+    --plot-density \
+    --density-cmap Purples \
+    --z-arg logplr \
+    --plot-contours \
+    --contour-color purple \
+    --plot-marginal \
+    --parameters "(primary_mass(mass1, mass2))/(secondary_mass(mass1, mass2)):\$q$" \
+                 chi_eff \
+    --contour-color "purple" \
+    --maxs "(primary_mass(mass1, mass2))/(secondary_mass(mass1, mass2)):2"
+
+
+
+pycbc_inference_plot_posterior --verbose \
+    --input-file /home/ysc/Documents/pycbc-inference-paper/run_files/GW150914/gw150914_posteriors_thinned.hdf \
+    --output-file iota_dl_gw150914.png \
+    --plot-density \
+    --density-cmap Purples \
+    --z-arg logplr \
+    --plot-contours \
+    --contour-color purple \
+    --plot-marginal \
+    --parameters "inclination*180/pi:$\iota$ (deg)" \
+                  distance \
+    --mins "inclination*180/pi:0" \
+    # --maxs "inclination*180/pi:180" \
